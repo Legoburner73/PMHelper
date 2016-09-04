082f58af48f9daf6306fb169f7b3dee170a33d44
Legoburner73 <legoplaysmcmail@gmail.com>
2016-09-04 20:00:14 +0100

    Update Main.php

--- src/Lego/PMHelper/Main.php
+++ src/Lego/PMHelper/Main.php
@@ -6,3 +6,18 @@ use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat as Colour;
 use pocketmine\Player;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+
+class Main extends PluginBase implements Listener
+{
+  private PREFIX = 
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    $this->getLogger()->info(Colour::YELLOW . " has joined the game.");
+  }
+  
+  public function onChatCommand(PlayerCommandPreprocessEvent $event){
+    $message = $event->getMessage();
+		$player = $event->getPlayer();
+  }
+}
