27c97ec81a7ac306c3a8c34a279820412f8e03f9
Legoburner73 <legoplaysmcmail@gmail.com>
2016-09-05 17:26:51 +0100

    Update Main.php

--- src/Lego/PMHelper/Main.php
+++ src/Lego/PMHelper/Main.php
@@ -10,7 +10,7 @@ use pocketmine\event\player\PlayerCommandPreprocessEvent;
 
 class Main extends PluginBase implements Listener
 {
-  private PREFIX = "§a[§2MVP+§a]§2 Emily";
+  private PREFIX = "§a[§2MVP+§a]§2 Emily §7>";
   private VERSION = "0.0.1"
   public function onEnable(){
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
@@ -23,13 +23,16 @@ class Main extends PluginBase implements Listener
     if(message{0} != "/") {
     	if(message{0} != "RP:") {
     		$needhelp = array("need", "help");
-    		$emilyversion = array("version", "emily");
+    		$emilyversion = array("version", "@emily");
+    		$languages = array("languages", "speak", "@emily");
     		$messagearray = str_split($message);
     		for($i = 0; $i < count($messagearray); $i++){
     			if(in_array($messagearray[$i], $needhelp)==true) {
-    				$player->sendMessage($this->PREFIX . Colour::LIGHT_GREY . "Need help? Try using /help for a list of commands.");
+    				$this->getServer()->broadcastMessage($this->PREFIX . "Need help? Try using /help for a list of commands.");
     			}elseif(in_array($messagearray[$1], $emilyversion)==true) {
-    			  $player->sendMessage($this->PREFIX . Colour::LIGHT_GREY . "My current version is: " . Colour::RED . $this->VERSION);
+    			  $this->getServer()->broadcastMessage($this->PREFIX . "My current version is: " . Colour::RED . $this->VERSION);
+    			}elseif(in_array($messagearray[$i], $languages)==true) {
+    			  $this->getServer()->broadcastMessage($this->PREFIX . "I can speak the following languages: English, French, Spanish and Potuguese.");
     			}
     	}
     }
